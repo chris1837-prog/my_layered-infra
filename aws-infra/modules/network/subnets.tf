cd38453b92ae925f76944a5069b69b8ffceb9639
evangelostsak <baggtsak55@gmail.com>
2025-08-28 19:28:22 +0200

    refactored vpc_id with variables

--- aws-infra/modules/network/subnets.tf
+++ aws-infra/modules/network/subnets.tf
@@ -4,7 +4,7 @@ resource "aws_subnet" "public" {
   # Loop over the az_configurations map
   for_each = var.az_configurations
 
-  vpc_id                  = aws_vpc.this.id
+  vpc_id                  = var.vpc_id
   availability_zone       = each.key # key is the AZ name (e.g. "eu-central-1a")
   cidr_block              = each.value.public_subnet_cidr
   map_public_ip_on_launch = true
@@ -18,7 +18,7 @@ resource "aws_subnet" "private" {
   # Loop directly over the az_configurations map
   for_each = var.az_configurations
 
-  vpc_id            = aws_vpc.this.id
+  vpc_id            = var.vpc_id
   availability_zone = each.key
   cidr_block        = each.value.private_subnet_cidr
 
