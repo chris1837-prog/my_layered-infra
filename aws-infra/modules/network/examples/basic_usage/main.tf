fe8e4ef947b6c03bb714bda4386b4e243d047827
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-08-28 20:41:22 +0200

    Terraform/provider versions moved to versions.tf

--- aws-infra/modules/network/examples/basic_usage/main.tf
+++ aws-infra/modules/network/examples/basic_usage/main.tf
@@ -3,16 +3,6 @@
 # Test configuration for the network module
 # =============================================================================
 
-terraform {
-  required_version = "~> 1.12.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.5.0"
-    }
-  }
-}
 
 provider "aws" {
   region  = "eu-central-1"
@@ -36,18 +26,18 @@ module "network" {
     Module      = "Network"
   }
 
-  allowed_admin_cidr       = "10.0.0.0/8"     # Example: wider range for testing
+  allowed_admin_cidr = "10.0.0.0/8" # Example: wider range for testing
   # Network configuration
   #cidr_block = "10.0.0.0/16"
 
-   # Single AZ configuration matching the architecture diagram
-    #az_configurations = {
-      # Use the first available AZ
-      #(data.aws_availability_zones.available.names[0]) = {
-        #public_subnet_cidr  = "10.0.1.0/24"  # For Edge VM (10.0.1.10)
-        #private_subnet_cidr = "10.0.2.0/24"  # For App VM (10.0.2.10)
-      #}
-    #}
+  # Single AZ configuration matching the architecture diagram
+  #az_configurations = {
+  # Use the first available AZ
+  #(data.aws_availability_zones.available.names[0]) = {
+  #public_subnet_cidr  = "10.0.1.0/24"  # For Edge VM (10.0.1.10)
+  #private_subnet_cidr = "10.0.2.0/24"  # For App VM (10.0.2.10)
+  #}
+  #}
 
   # Security configuration (override defaults for testing)
 }
