9f023851a0bf6d0721551fb0eec3ad3e57127bc0
adrianalola <adryland79@gmail.com>
2025-08-28 20:58:43 +0200

    feat(network): add VPC (vpc.tf), variables, and vpc_id output

---
+++ aws-infra/modules/network/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  description = "Project name used for resource naming"
+  type        = string
+}
+
+variable "common_tags" {
+  description = "Common tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
