16e20d15d738600e4230613780482dd56a868718
evangelostsak <baggtsak55@gmail.com>
2025-08-29 09:32:15 +0200

    outputs conflict fix 1.1

--- aws-infra/modules/network/outputs.tf
+++ aws-infra/modules/network/outputs.tf
@@ -2,13 +2,8 @@ output "vpc_id" {
   description = "The ID of the VPC"
   value       = aws_vpc.this.id
 }
-
-<<<<<<< HEAD
 output "igw_id" {
   description = "The ID of the Internet Gateway"
   value       = aws_internet_gateway.this.id
 }
 
-
-=======
->>>>>>> origin/feat/network_module
