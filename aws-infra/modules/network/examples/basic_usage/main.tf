2b02f8013481732545d05edea0c87a822c85643a
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-08-29 12:24:46 +0200

    Fixed mismatch in cidr blocks

--- aws-infra/modules/network/examples/basic_usage/main.tf
+++ aws-infra/modules/network/examples/basic_usage/main.tf
@@ -25,16 +25,17 @@ module "network" {
     Module      = "Network"
   }
 
+  vpc_cidr           = "10.0.0.0/16"
   allowed_admin_cidr = "10.0.0.0/8" # Example: wider range for testing
-  # Network configuration
-  #cidr_block = "10.0.0.0/16"
+
+
 
   # Single AZ configuration matching the architecture diagram
-  #az_configurations = {
+  az_configurations = {
     # Use the first available AZ
-    #(data.aws_availability_zones.available.names[0]) = {
-      #public_subnet_cidr  = "10.0.1.0/24"  # For Edge VM (10.0.1.10)
-      #private_subnet_cidr = "10.0.2.0/24"  # For App VM (10.0.2.10)
-    #}
-  #}
+    (data.aws_availability_zones.available.names[0]) = {
+      public_subnet_cidr  = "10.0.1.0/24" # For Edge VM (10.0.1.10)
+      private_subnet_cidr = "10.0.2.0/24" # For App VM (10.0.2.10)
+    }
+  }
 }
