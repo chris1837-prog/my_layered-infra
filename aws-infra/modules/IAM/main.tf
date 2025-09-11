00a6368cdba2e17154883eb4d991c5c95e087aa8
evangelostsak <baggtsak55@gmail.com>
2025-09-11 11:23:24 +0200

    Changed deprecated attribute data.aws_region.(current.name) to (current.id)

--- aws-infra/modules/IAM/main.tf
+++ aws-infra/modules/IAM/main.tf
@@ -24,7 +24,7 @@ data "aws_iam_policy_document" "remote_state_access" {
 			"dynamodb:UpdateItem"
 		]
 		resources = [
-			"arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/${var.lock_table}"
+			"arn:aws:dynamodb:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:table/${var.lock_table}"
 		]
 	}
 }
