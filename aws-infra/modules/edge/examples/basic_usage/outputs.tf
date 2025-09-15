bcbe0d4191a7bcf22fcc74f4316ab469405d0a90
evangelostsak <baggtsak55@gmail.com>
2025-09-15 23:19:12 +0200

    Updated examples basic_usage to be incline with the latest changes

--- aws-infra/modules/edge/examples/basic_usage/outputs.tf
+++ aws-infra/modules/edge/examples/basic_usage/outputs.tf
@@ -2,3 +2,13 @@ output "edge_public_ip" {
   description = "The public IP of the Edge VM. Use this for SSH and DNS."
   value       = module.edge.edge_public_ip
 }
+
+output "edge_private_key_path" {
+  description = "Path to the generated private SSH key for the Edge VM. Use this to connect as the admin user."
+  value       = module.edge.edge_private_key_path
+}
+
+output "edge_instance_id" {
+  description = "The ID of the created Edge VM instance."
+  value       = module.edge.edge_instance_id
+}
