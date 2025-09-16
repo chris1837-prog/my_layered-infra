9736585edb869ff3e421a1cf5af8420b9a022693
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-09-16 17:59:23 +0200

    Add variables for instance profile

--- aws-infra/modules/iam/variables.tf
+++ aws-infra/modules/iam/variables.tf
@@ -13,3 +13,19 @@ variable "engineer_role_name" {
   type        = string
   default     = "engineer-role"
 }
+
+variable "project_name" {
+  description = "Project name (used for naming IAM resources)"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment (dev, staging, prod, etc.)"
+  type        = string
+}
+
+variable "common_tags" {
+  description = "A map of tags to apply to IAM resources"
+  type        = map(string)
+  default     = {}
+}
