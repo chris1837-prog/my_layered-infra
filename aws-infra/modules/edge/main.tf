f2db2b3380e7287247a22becd731caf8fe0cc619
evangelostsak <baggtsak55@gmail.com>
2025-09-17 22:00:48 +0200

    added right value for ami

--- aws-infra/modules/edge/main.tf
+++ aws-infra/modules/edge/main.tf
@@ -2,27 +2,8 @@ data "aws_ssm_parameter" "ubuntu" {
   name = local.ubuntu_ssm_path
 }
 
-# # --- Dynamic SSH Key Generation ---
-# resource "tls_private_key" "edge" {
-#   algorithm = "RSA"
-#   rsa_bits  = 2048
-# }
-
-# resource "aws_key_pair" "edge" {
-#   key_name   = "${var.project_name}-${var.environment}-edge-key"
-#   public_key = tls_private_key.edge.public_key_openssh
-# }
-
-# # Save private key locally for admin use
-# resource "local_file" "edge_private_key" {
-#   content         = tls_private_key.edge.private_key_pem
-#   filename        = abspath("${path.module}/edge_private_key.pem")
-#   file_permission = "0600"
-# }
-
-
 resource "aws_instance" "edge" {
-  ami                         = data.aws_ami.ubuntu.id
+  ami                         = data.aws_ssm_parameter.ubuntu.value
   instance_type               = var.instance_type
   subnet_id                   = var.public_subnet_id
   vpc_security_group_ids      = [var.sg_edge_id]
