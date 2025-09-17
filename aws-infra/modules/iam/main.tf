77e4d43979a128a78b99287fd8d85e10e065ec0d
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-09-17 23:48:14 +0200

    Update main.tf

--- aws-infra/modules/iam/main.tf
+++ aws-infra/modules/iam/main.tf
@@ -82,7 +82,7 @@ resource "aws_iam_role" "ec2_role" {
   })
 
   tags = merge(var.common_tags, {
-    Name = "${var.project_name}-${var.environment}-appdb-role"
+    Name = "${var.project_name}-${var.environment}-ec2-role"
   })
 }
 
@@ -91,7 +91,7 @@ resource "aws_iam_role" "ec2_role" {
 # -------------------------
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "${var.project_name}-${var.environment}-ec2-instance-profile"
-  role = aws_iam_role.appdb_role.name
+  role = aws_iam_role.ec2_role.name
 
   tags = merge(var.common_tags, {
     Name = "${var.project_name}-${var.environment}-ec2-instance-profile"
