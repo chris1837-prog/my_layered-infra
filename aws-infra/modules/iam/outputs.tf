797bc869748afe7a6221c14144e8bc11c8fd4b61
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-09-17 23:34:14 +0200

    Naming for instance profile updated  appdb --> ec2

--- aws-infra/modules/iam/outputs.tf
+++ aws-infra/modules/iam/outputs.tf
@@ -3,7 +3,7 @@ output "engineer_role_arn" {
   value       = aws_iam_role.engineer.arn
 }
 
-output "appdb_instance_profile_name" {
+output "ec2_instance_profile_name" {
   description = "Instance profile name for AppDB EC2"
   value       = aws_iam_instance_profile.ec2_instance_profile.name
 }
