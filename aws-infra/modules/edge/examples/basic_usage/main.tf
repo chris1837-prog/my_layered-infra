95082c449114c01a860625c02417d59b2c152be0
evangelostsak <baggtsak55@gmail.com>
2025-09-19 00:47:19 +0200

    add a password for registry for testing

--- aws-infra/modules/edge/examples/basic_usage/main.tf
+++ aws-infra/modules/edge/examples/basic_usage/main.tf
@@ -224,6 +224,7 @@ module "edge" {
   vpc_id                    = aws_vpc.test_vpc.id
   registry_domain    = "registry.edge.example.com"
   registry_user      = "registry"
+  registry_password  = "registry"
   registry_zone_name = "edge.example.com"
   instance_type             = local.instance_type
   sg_edge_id                = aws_security_group.edge.id
