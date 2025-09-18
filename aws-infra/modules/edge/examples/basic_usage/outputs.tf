633b27ff476f46211f41c49ce6c6543b7e48483e
evangelostsak <baggtsak55@gmail.com>
2025-09-19 00:47:31 +0200

    output registry url

--- aws-infra/modules/edge/examples/basic_usage/outputs.tf
+++ aws-infra/modules/edge/examples/basic_usage/outputs.tf
@@ -12,3 +12,8 @@ output "edge_instance_id" {
   description = "The ID of the created Edge VM instance."
   value       = module.edge.edge_instance_id
 }
+
+output "registry_url" {
+  description = "The URL of the Docker registry (proxied by Caddy)."
+  value       = "https://${module.edge.registry_domain}"
+}
