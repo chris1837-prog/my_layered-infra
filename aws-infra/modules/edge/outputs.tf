abc91f3e07c7330602cc42d4d37beaaa9bb8a87a
evangelostsak <baggtsak55@gmail.com>
2025-09-18 12:36:37 +0200

    added edge_private_ip as output

--- aws-infra/modules/edge/outputs.tf
+++ aws-infra/modules/edge/outputs.tf
@@ -1,3 +1,17 @@
+output "edge_private_ip" {
+  description = "The private IP address of the Edge VM. Use this for internal DNS and registry A record."
+  value       = aws_instance.edge.private_ip
+}
+
+# output "registry_domain" {
+#   description = "The FQDN of the Docker registry."
+#   value       = var.registry_domain
+# }
+#
+# output "registry_zone_id" {
+#   description = "The Route 53 private hosted zone ID for the registry."
+#   value       = aws_route53_zone.registry_private.zone_id
+# }
 output "edge_instance_id" {
   description = "The ID of the created Edge VM instance."
   value       = aws_instance.edge.id
