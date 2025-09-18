50cfed6acfa5104db55138aa6fe179f165941e43
evangelostsak <baggtsak55@gmail.com>
2025-09-19 00:51:20 +0200

    uncomment registry domain and zone id outputs

--- aws-infra/modules/edge/outputs.tf
+++ aws-infra/modules/edge/outputs.tf
@@ -3,15 +3,15 @@ output "edge_private_ip" {
   value       = aws_instance.edge.private_ip
 }
 
-# output "registry_domain" {
-#   description = "The FQDN of the Docker registry."
-#   value       = var.registry_domain
-# }
-#
-# output "registry_zone_id" {
-#   description = "The Route 53 private hosted zone ID for the registry."
-#   value       = aws_route53_zone.registry_private.zone_id
-# }
+output "registry_domain" {
+  description = "The FQDN of the Docker registry."
+  value       = var.registry_domain
+}
+
+output "registry_zone_id" {
+  description = "The Route 53 private hosted zone ID for the registry."
+  value       = aws_route53_zone.registry_private.zone_id
+}
 output "edge_instance_id" {
   description = "The ID of the created Edge VM instance."
   value       = aws_instance.edge.id
