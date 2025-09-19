a1b84d178ba4c3bc4bd01320e0f68c8e96b7638b
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-09-19 20:07:55 +0200

    Store non-sensitive configuration as String parameters

--- aws-infra/bootstrap/dev/ssm_parameter.tf
+++ aws-infra/bootstrap/dev/ssm_parameter.tf
@@ -25,3 +25,39 @@ resource "aws_ssm_parameter" "registry_password" {
   value       = random_password.registry_password.result
   description = "Private Docker registry password"
 }
+
+# Store non-sensitive configuration as String parameters
+resource "aws_ssm_parameter" "registry_url" {
+  name        = "/${var.project_name}/${var.environment}/registry_url"
+  type        = "String"
+  value       = var.registry_url
+  description = "Private Docker registry URL"
+}
+
+resource "aws_ssm_parameter" "registry_user" {
+  name        = "/${var.project_name}/${var.environment}/registry_user"
+  type        = "String"
+  value       = var.registry_user
+  description = "Private Docker registry username"
+}
+
+resource "aws_ssm_parameter" "app_image_tag" {
+  name        = "/${var.project_name}/${var.environment}/app_image_tag"
+  type        = "String"
+  value       = var.app_image_tag
+  description = "Docker image tag for the application"
+}
+
+resource "aws_ssm_parameter" "postgres_db" {
+  name        = "/${var.project_name}/${var.environment}/postgres_db"
+  type        = "String"
+  value       = var.postgres_db
+  description = "PostgreSQL database name"
+}
+
+resource "aws_ssm_parameter" "postgres_user" {
+  name        = "/${var.project_name}/${var.environment}/postgres_user"
+  type        = "String"
+  value       = var.postgres_user
+  description = "PostgreSQL username"
+}
