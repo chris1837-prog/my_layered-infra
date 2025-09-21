49c6fb4f702daa54a5feddb1d8ff5609f4940821
evangelostsak <baggtsak55@gmail.com>
2025-09-21 11:07:59 +0200

    deleted registry password and user creation logic from locals (ssm related)

--- aws-infra/modules/edge/locals.tf
+++ aws-infra/modules/edge/locals.tf
@@ -5,14 +5,4 @@ locals {
 
   # Build SSM parameter path dynamically
   ubuntu_ssm_path = "/aws/service/canonical/ubuntu/server/${var.ubuntu_version}/stable/current/${local.arch}/hvm/ebs-gp2/ami-id"
-
-  # Build SSM parameter names for registry credentials dynamically
-  registry_password_ssm_path = "/edge/registry/${var.environment}/password"  # maybe not
-  registry_user_ssm_path     = "/edge/registry/${var.environment}/username"
-
-  # Build final registry password
-  registry_password_final = var.registry_password != null ? var.registry_password : random_password.registry.result
-
-  # bcrypt hash of the final registry password
-  bcrypt_hash = bcrypt(local.registry_password_final)
 }
