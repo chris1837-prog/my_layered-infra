36b0539bd38d59eea7cf9e1745839fe4f3ecc4ad
evangelostsak <baggtsak55@gmail.com>
2025-09-22 11:24:59 +0200

    terraform fmt

--- aws-infra/modules/edge/examples/basic_usage/main.tf
+++ aws-infra/modules/edge/examples/basic_usage/main.tf
@@ -222,10 +222,10 @@ module "edge" {
   project_name              = "layered-infra"
   environment               = "dev"
   vpc_id                    = aws_vpc.test_vpc.id
-  registry_domain    = "registry.edge.example.com"
-  registry_user      = "registry"
-  registry_password  = "registry"
-  registry_zone_name = "edge.example.com"
+  registry_domain           = "registry.edge.example.com"
+  registry_user             = "registry"
+  registry_password         = "registry"
+  registry_zone_name        = "edge.example.com"
   instance_type             = local.instance_type
   sg_edge_id                = aws_security_group.edge.id
   ubuntu_version            = local.ubuntu_version
