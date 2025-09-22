1d9de689108884d73188bf1689a852ef81ddd245
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-09-23 00:31:33 +0200

    Update locals.tf

--- aws-infra/environments/dev/locals.tf
+++ aws-infra/environments/dev/locals.tf
@@ -6,13 +6,14 @@ locals {
     project_name            = local.bootstrap_outputs.project_name
     environment             = local.bootstrap_outputs.environment
     aws_region              = local.bootstrap_outputs.aws_region
-    eip_allocation_id = local.bootstrap_outputs.edge_eip_allocation_id
-    edge_private_ip   = local.bootstrap_outputs.edge_private_ip
+    eip_allocation_id       = local.bootstrap_outputs.edge_eip_allocation_id
+    edge_private_ip         = local.bootstrap_outputs.edge_private_ip
     parameter_paths = {
       external_registry_url = local.ssm_parameters.parameter_paths.external_registry_url
+      internal_registry_url = local.ssm_parameters.parameter_paths.internal_registry_url
       registry_user         = local.ssm_parameters.parameter_paths.registry_user
       registry_password     = local.ssm_parameters.parameter_paths.registry_password
-      internal_registry_url = local.ssm_parameters.parameter_paths.internal_registry_url
+
     }
   }
 
