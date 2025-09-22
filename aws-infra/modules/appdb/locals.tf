ca1ce81b7743ff9b96ae09506ac4d396962a1aa3
Mazdaratti <vasya.mazdaratti@googlemail.com>
2025-09-22 18:07:39 +0200

    Update locals with enforced tags

--- aws-infra/modules/appdb/locals.tf
+++ aws-infra/modules/appdb/locals.tf
@@ -6,3 +6,13 @@ locals {
   # Build SSM parameter path dynamically
   ubuntu_ssm_path = "/aws/service/canonical/ubuntu/server/${var.ubuntu_version}/stable/current/${local.arch}/hvm/ebs-gp2/ami-id"
 }
+
+locals {
+  enforced_tags = {
+    Project     = var.project_name
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+
+  merged_tags = merge(var.common_tags, local.enforced_tags)
+}
