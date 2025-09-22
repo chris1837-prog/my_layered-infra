02d321744c401ee82b3b15a872c26634a85952a1
evangelostsak <baggtsak55@gmail.com>
2025-09-22 11:22:54 +0200

    Refactor outputs in edge module: remove unused outputs and update public IP reference

--- aws-infra/modules/edge/outputs.tf
+++ aws-infra/modules/edge/outputs.tf
@@ -1,17 +1,8 @@
-output "edge_private_ip" {
-  description = "The private IP address of the Edge VM. Use this for internal DNS and registry A record."
-  value       = aws_instance.edge.private_ip
+output "primary_network_interface_id" {
+  description = "The primary network interface ID of the Edge VM instance."
+  value       = aws_instance.edge.primary_network_interface_id
 }
 
-output "registry_domain" {
-  description = "The FQDN of the Docker registry."
-  value       = var.registry_domain
-}
-
-output "registry_zone_id" {
-  description = "The Route 53 private hosted zone ID for the registry."
-  value       = aws_route53_zone.registry_private.zone_id
-}
 output "edge_instance_id" {
   description = "The ID of the created Edge VM instance."
   value       = aws_instance.edge.id
@@ -19,5 +10,5 @@ output "edge_instance_id" {
 
 output "edge_public_ip" {
   description = "The public IP address of the Edge VM. Use this to connect via SSH."
-  value       = aws_eip.edge.public_ip
+  value       = aws_eip_association.eip_assoc[0].public_ip
 }
