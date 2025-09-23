3c4de62fbebd8ef4b09fc7e3d269d164f98bb23f
evangelostsak <baggtsak55@gmail.com>
2025-09-23 15:50:16 +0200

    feat: add outputs for Docker registry external and internal URLs

--- aws-infra/modules/edge/outputs.tf
+++ aws-infra/modules/edge/outputs.tf
@@ -12,3 +12,13 @@ output "edge_public_ip" {
   description = "The public IP address of the Edge VM. Use this to connect via SSH."
   value       = length(aws_eip_association.eip_assoc) > 0 ? aws_eip_association.eip_assoc[0].public_ip : null
 }
+
+output "registry_external_url" {
+  description = "The external URL of the Docker registry (proxied by Caddy)."
+  value       = var.registry_external_url
+}
+
+output "registry_internal_url" {
+  description = "The internal URL of the Docker registry (proxied by Caddy)."
+  value       = var.registry_internal_url
+}
