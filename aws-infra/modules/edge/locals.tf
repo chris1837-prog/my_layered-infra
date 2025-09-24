f08936c5c2520b798997ec6ed616af5460c976dc
evangelostsak <baggtsak55@gmail.com>
2025-09-24 14:29:51 +0200

    wrong commit

--- aws-infra/modules/edge/locals.tf
+++ aws-infra/modules/edge/locals.tf
@@ -2,6 +2,7 @@ locals {
   # Detect architecture from instance type
   # ARM = Graviton families (t4g, m6g, c7g, etc.), otherwise AMD64
   arch = can(regex(".*g\\.", var.instance_type)) ? "arm64" : "amd64"
+  hello
 
   # Build SSM parameter path dynamically
   ubuntu_ssm_path = "/aws/service/canonical/ubuntu/server/${var.ubuntu_version}/stable/current/${local.arch}/hvm/ebs-gp2/ami-id"
