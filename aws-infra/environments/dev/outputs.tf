b096de767569087c72daec4d938749820232eaeb
Jan-Philipp <jan-philipp@pop-os.fritz.box>
2025-10-10 14:39:39 +0200

    Read app domain from TF outputs; add app_domain to /dev.outputs.tf

--- aws-infra/environments/dev/outputs.tf
+++ aws-infra/environments/dev/outputs.tf
@@ -11,3 +11,8 @@ output "budget_dev_topic_arn" {
   description = "SNS Topic ARN for DEV budget alerts"
   value       = module.budget_dev.sns_topic_arn
 }
+
+output "app_domain" {
+  description = "Public domain for the app (edge layer)"
+  value = module.edge.primary_domain
+}
