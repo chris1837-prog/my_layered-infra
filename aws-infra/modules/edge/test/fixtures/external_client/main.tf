abbfc51b0dd9967dfbbbe30a963413cbfa1dd351
Sivaneswaran Sutharsan <s.sutharsan@outlook.de>
2025-10-13 21:47:32 +0200

    Fix Terraform formatting (G1)

--- aws-infra/modules/edge/test/fixtures/external_client/main.tf
+++ aws-infra/modules/edge/test/fixtures/external_client/main.tf
@@ -33,7 +33,7 @@ resource "aws_vpc" "vpc" {
   cidr_block           = "10.200.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
-  tags = { Name = local.name }
+  tags                 = { Name = local.name }
 }
 
 resource "aws_internet_gateway" "igw" {
