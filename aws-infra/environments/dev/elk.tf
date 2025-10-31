82e8d6d4c0413e586ddae701d8fe6d1839b76413
FatouBadarouS <fat.salao@gmail.com>
2025-10-31 12:54:53 +0100

    Dedicated_elk_stack_vm

--- aws-infra/environments/dev/elk.tf
+++ aws-infra/environments/dev/elk.tf
@@ -1,8 +1,30 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical (official Ubuntu images)
+}
+
+
 #Instance Profile 
 
 module "elk_vm_iam" {
   source = "../../modules/iam"
-  name   = "elk-vm-profile"
+  project_name = var.project_name
+  environment  = var.environment
+  common_tags  = local.common_tags
+
+  tf_state_bucket = local.bootstrap_outputs.tf_state_bucket_name
+  lock_table      = local.bootstrap_outputs.tf_state_lock_table
 }
 
 #aws-security-group 
@@ -17,7 +39,7 @@ resource "aws_security_group" "elk_vm_sg" {
     from_port       = 5044
     to_port         = 5044
     protocol        = "tcp"
-    security_groups = [aws_security_group.edge_sg.id, aws_security_group.app_sg.id]
+    security_groups = [module.network.sg_edge_id, module.network.sg_app_id]
   }
 
   ingress {
@@ -25,7 +47,7 @@ resource "aws_security_group" "elk_vm_sg" {
     from_port       = 5601
     to_port         = 9200
     protocol        = "tcp"
-    security_groups = [aws_security_group.edge_sg.id]
+    security_groups = [module.network.sg_edge_id]
   }
 
   egress {
@@ -42,8 +64,8 @@ resource "aws_instance" "obs_elk_vm" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.large"
   subnet_id              = module.network.private_subnet_ids[0]
-  key_name               = outputs.generated_key
-  iam_instance_profile   = module.elk_vm_iam.instance_profile_name
+  key_name               = aws_key_pair.generated_key.key_name
+  iam_instance_profile   = module.elk_vm_iam.ec2_instance_profile_name
   vpc_security_group_ids = [aws_security_group.elk_vm_sg.id]
 
   user_data = file("${path.module}/cloud-init-elk.tpl")
@@ -56,5 +78,6 @@ resource "aws_instance" "obs_elk_vm" {
 
 #cloud-init/user_data requirements: 
 #Install Docker (Docker Engine) and Docker Compose plugin (or docker-compose v2).
-#Create /opt/elk/ and write docker-compose.elk.yml and logstash.conf. 
-#Start docker compose
+
+
+  
